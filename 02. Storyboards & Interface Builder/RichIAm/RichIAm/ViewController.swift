ed5cb75458a735dc9e2d41a94bbb3999877824c4
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-01-13 18:10:11 +0100

    feat: :rocket: Initialize RichIAm project with essential files and configurations

---
+++ 02. Storyboards & Interface Builder/RichIAm/RichIAm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RichIAm
+//
+//  Created by Burak Balci on 13/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
