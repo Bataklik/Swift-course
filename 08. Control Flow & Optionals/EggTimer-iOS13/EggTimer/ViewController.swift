1ecb11341525c71b9ce7fca3158d36eca059f614
Bataklik <60544395+Bataklik@users.noreply.github.com>
2026-01-22 18:39:46 +0100

    feat: :sparkles: Implement egg timer functionality with UI updates and progress tracking

--- 08. Control Flow & Optionals/EggTimer-iOS13/EggTimer/ViewController.swift
+++ 08. Control Flow & Optionals/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+  let eggTimer = [
+    "Soft": (1 * 60),
+    "Medium": (8 * 60),
+    "Hard": (12 * 60),
+  ]
 
+  @IBOutlet weak var countLabel: UILabel!
+  @IBOutlet weak var timeBar: UIProgressView!
+
+  var eggSeconds = 1
+  var timer = Timer()
+
+  @IBAction func eggPressed(_ sender: UIButton) {
+    let eggHardness = sender.currentTitle! as String
+
+    eggSeconds = Int(eggTimer[eggHardness]!)
+    timeBar.setProgress((Float(eggSeconds) / 60), animated: true)
+
+    timer = Timer.scheduledTimer(timeInterval: 1.0,
+                         target: self,
+                         selector: #selector(updateTimer),
+                         userInfo: nil,
+                         repeats: true)
+
+    }
+  @objc func updateTimer(){
+    if eggSeconds > 0{
+      countLabel.text = String(eggSeconds) + " Seconds"
+      timeBar.setProgress((Float(eggSeconds) / 60), animated: true)
+      eggSeconds -= 1
+    }else{
+      timer.invalidate()
+      countLabel.text = "Done"
+      timeBar.setProgress(0.0, animated: true)
+    }
+  }
 
 }
